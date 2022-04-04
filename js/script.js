c5a80c4a7617cffbe52d2b2a16dc24252110839c
kyliestran <93623702+kyliestran@users.noreply.github.com>
2022-04-03 21:12:49 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,35 +1,27 @@
-const para = document.querySelector('query')
-para.addEventListener('click', updateName);
-function updateName (){
-  const name = prompt('Enter a new name');
-  para.textContent = 'Player 1: ${name}';
-}
+var index = 0;
+    change();
 
-let slideIndex = 1;
-showSlides(slideIndex);
+    function change() {
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+        //Collect all images with class 'slides'
+        var x = document.getElementsByClassName('slides');
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+        //Set all the images display to 'none' (invisible)
+        for(var i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+        //Increment index variable
+        index++;
+
+        //Set index to 1 if it's greater than the amount of images
+        if(index > x.length) {
+            index = 1;
+        }
+
+        //set image display to 'block' (visible)
+        x[index - 1].style.display = "block";
+
+        //set loop to change image every 3000 milliseconds (3 seconds)
+        setTimeout(change, 3000);
+    }
